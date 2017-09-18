bc820517a8d407b9fd6f80826ffb9c2771f5d175
greatghoul <greatghoul@gmail.com>
2017-09-18 23:18:21 +0800

    Implement login in activity
    - when login successful, store user credentials in shared preferences,
    - when login failed, show error message through toast.

--- app/src/main/java/com/ghoulmind/habitdrod/LoginActivity.kt
+++ app/src/main/java/com/ghoulmind/habitdrod/LoginActivity.kt
@@ -1,5 +1,7 @@
 package com.ghoulmind.habitdrod
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -7,6 +9,9 @@ import com.ghoulmind.habitica.HabiticaClient
 import com.ghoulmind.habitica.HabiticaException
 
 import kotlinx.android.synthetic.main.activity_login.*
+import android.os.AsyncTask
+import org.json.JSONObject
+
 
 /**
  * A login screen that offers login via email/password.
@@ -28,12 +33,38 @@ class LoginActivity : AppCompatActivity() {
         var username = inputUsername.text.toString()
         var password = inputPassword.text.toString()
 
-        try {
-            val data = apiClient.login(username, password)
-            // TODO: Store api key and user id
-        } catch (e: HabiticaException) {
-            // TODO: Show user the errors
+        UserLoginTask(username, password).execute()
+    }
+
+    private fun getPrefsEditor(): SharedPreferences.Editor {
+        val prefs = getSharedPreferences(getString(R.string.app_prefs_file), Context.MODE_PRIVATE)
+        return prefs.edit()
+    }
+
+    private fun saveUserCredentials(id: String, apiToken: String) {
+        val editor = getPrefsEditor()
+        editor.putString("id", id)
+        editor.putString("apiToken", apiToken)
+        editor.commit()
+    }
+
+    inner class UserLoginTask internal constructor(private val username: String, private val password: String) : AsyncTask<Void, Void, JSONObject?>() {
+        override fun doInBackground(vararg params: Void): JSONObject? {
+            try {
+                return apiClient.login(username, password)
+            } catch (e: HabiticaException) {
+                return null
+            }
         }
 
+        override fun onPostExecute(data: JSONObject?) {
+            if (data == null) {
+                inputPassword.requestFocus()
+                Toast.makeText(applicationContext, R.string.error_signin_invalid, Toast.LENGTH_LONG).show()
+            } else {
+                saveUserCredentials(data.getString("id"), data.getString("apiToken"))
+                Toast.makeText(applicationContext, "Welcome", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
